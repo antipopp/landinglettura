3cee78ea6a9a5e40b96e1843ceb2e3a30958768e
Francesco Cartier <francescocartier@gmail.com>
2025-08-25 13:54:33 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,32 +6,48 @@ import Script from "next/script";
 import type React from "react";
 import "./globals.css";
 import { FB_PIXEL_ID } from "@/lib/fpixel";
-import FacebookPixel from "@/components/FacebookPixel";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Carlo Cartier | Corso di Lettura a Voce Alta",
-	description: "Trasforma la Tua Lettura in Arte con Carlo Cartier",
+  title: "Carlo Cartier | Corso di Lettura a Voce Alta",
+  description: "Trasforma la Tua Lettura in Arte con Carlo Cartier",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="it">
-			<Script
-				type="text/javascript"
-				src="//embeds.iubenda.com/widgets/27d1dbfd-187e-4e3a-b0a8-46f126799b7d.js"
-			/>
-			<body className={inter.className}>
-				{children}
-				<FacebookPixel />
-				<Toaster />
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="it">
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${FB_PIXEL_ID}');
+fbq('track', 'PageView');
+`,
+        }}
+      />
+      <Script
+        type="text/javascript"
+        src="//embeds.iubenda.com/widgets/27d1dbfd-187e-4e3a-b0a8-46f126799b7d.js"
+      />
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
